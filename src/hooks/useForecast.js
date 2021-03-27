898b9ffece16d28bf510297512f7043c94c5fb5e
hoangvuong.tran21 <hoangvuong.tran21@gmail.com>
2021-03-27 07:14:19 +0700

    Change from proxy to url

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import { useState } from 'react';
 
+// https://www.metaweather.com/api/
+const instance = axios.create({
+  baseURL: 'https://www.metaweather.com/api/'
+});
+
 export const useForecast = () => {
   const [loading, setLoading] = useState(false);
   const [forecasts, setForecasts] = useState([]);
@@ -14,10 +19,10 @@ export const useForecast = () => {
     const urlLocation = `location/search/?query=${searchText}`;
 
     try {
-      const { data } = await axios(urlLocation);
+      const { data } = await instance(urlLocation);
       if (data && data.length > 0) {
         const urlForecast = `location/${data[0]['woeid']}/`;
-        const response = await axios(urlForecast);
+        const response = await instance(urlForecast);
         const { data: forecastData } = response;
         const { consolidated_weather } = forecastData;
 
